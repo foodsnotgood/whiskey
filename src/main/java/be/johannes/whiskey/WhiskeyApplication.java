119259594f9f667a0925c6405099c4bb56338f5d
foodsnotgood <joroeder2588@gmail.com>
2021-11-25 18:41:34 +0100

    first commit

---
+++ src/main/java/be/johannes/whiskey/WhiskeyApplication.java
@@ -0,0 +1,13 @@
+package be.johannes.whiskey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiskeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/johannes/whiskey/WhiskeyApplicationTests.java
@@ -0,0 +1,13 @@
+package be.johannes.whiskey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
